fa22cf79f995fda7102033ae43e9663e3f6180d8
Cristian <cristian.mg.david@gmail.com>
2025-09-01 12:45:21 -0300

    feat(Process): crear nueva clase - Crear clase 'Process', agregar metodo 'getProcess' y 'toString'

--- src/Process.java
+++ src/Process.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Process {
+    String processName;
+    int arrivalTime, cantBurstCPU, timeBurstCPU, timerBurstIO, priority;
+
+    Process(String processName, int arrivalTime, int cantBurstCPU, int timerBurstCPU, int timerBurstIO, int priority) {
+        this.processName = processName;
+        this.arrivalTime = arrivalTime;
+        this.cantBurstCPU = cantBurstCPU;
+        this.timeBurstCPU = timerBurstCPU;
+        this.timerBurstIO = timerBurstIO;
+        this.priority = priority;
+    }
+
+    public Process getProcess() {
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Process{" +
+                "name='" + processName + '\'' +
+                ", arrival=" + arrivalTime +
+                ", cantBurstCPU=" + cantBurstCPU +
+                ", timeBurstCPU=" + timeBurstCPU +
+                ", timeBurstIO=" + timerBurstIO +
+                ", priority=" + priority +
+                '}';
+    }
 }
